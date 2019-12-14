006629be329aea5b60c78ab3f22bef0d35283f29
Mike <mexvance04@gmail.com>
2019-12-13 18:34:43 -0700

    Updates

---
+++ precache-manifest.a7cbfaec97020f96e9372ba2b3fffa3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317619a13303712fbecfb22fb9bbecbc",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7779bd9eb36572056c73",
+    "url": "/my-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46dfed47639caeb3e352",
+    "url": "/my-portfolio/static/js/2.9f4388f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-portfolio/static/js/2.9f4388f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7779bd9eb36572056c73",
+    "url": "/my-portfolio/static/js/main.845df11f.chunk.js"
+  },
+  {
+    "revision": "4184a7eacb7c48bd5c06",
+    "url": "/my-portfolio/static/js/runtime-main.4a96037b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);

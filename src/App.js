8a37fe9b8c5aa18f3d063257c4d00adf8c2ac3b7
Mike <mexvance04@gmail.com>
2019-12-19 00:16:11 -0700

    updated project main settings

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Portfolio, More to come soon!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/mexvance/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my Github!
         </a>
       </header>
     </div>

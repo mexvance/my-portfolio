2b3164d6e72bcdb2aaef3b794e67d1b90a919dae
Mike <mexvance04@gmail.com>
2020-02-09 15:29:35 -0700

    removed paralax and moved picture to about section

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -4,36 +4,54 @@ import '../App.css';
 import '../css/homepage.css';
 import { MdArrowForward } from "react-icons/md";
 const HomePage = props =>{
+  
+  const ref = React.createRef();
   const paralaxMove = event =>{
     let xPos = event.pageX;
     let yPos = event.pageY;
     event.currentTarget.style.backgroundPosition = -xPos/4 + "px " + -yPos/4 + "px," +
                                                    xPos/12 + "px " + yPos/12 + "px";
   } 
-    return(
-        <>
-        <div className="part-00">
-          <div id="part-01" className="part-01" onMouseMove={event=>paralaxMove(event)}>
-            <div className="Header-Title">
-              <p className="title">
-                Hi, I'm <span>Mike Vance</span> 
-              </p>
-              <p className="title2">I'm a Software Developer</p>
-              <button>
-                About Me 
-                <span className="arrowIcon">
-                  <MdArrowForward/>
-                </span>
-              </button>
+  const handleClick = () =>{
+    ref.current.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });;
+    // event.current.scrollIntoView({
+    //   behavior: 'smooth',
+    //   block: 'start',
+    // });
+  };
+  return(
+      <>
+      <div className="part-00">
+        <div id="part-01" className="part-01" onMouseMove={ref=>paralaxMove(ref)}>
+        
+          <div className="Header-Title">
+          
+            <p className="title">
+              Hi, I'm <span>Mike Vance</span> 
+            </p>
+            <p className="title2">I'm a Software Developer</p>
+            <div href="about" className="button" type="button"
+            onClick={(event) => handleClick(event)}>
+              About Me 
+              <span className="arrowIcon">
+                <MdArrowForward className="icon"/>
+              </span>
             </div>
           </div>
         </div>
-        <div className="About-Me">
-        <p>About Me</p>
-        <img src={logo} className="App-logo" alt="logo"/>
-        </div>
-        </>
-    );
+      </div>
+      <div id="about" ref={ref} className="About-Me">
+      <h1>About Me</h1>
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p></p>
+     
+
+      </div>
+      </>
+  );
 };
 
 export default HomePage;
